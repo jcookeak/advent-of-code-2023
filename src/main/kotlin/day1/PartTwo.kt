cf957d9864faf6f3adc619fa56cde34d0bbf306e
jcookeak <j.cookeakaiwa@gmail.com>
2023-12-06 21:09:28 -0500

    day 1 prompt

---
+++ src/main/kotlin/day1/PartTwo.kt
@@ -0,0 +1,50 @@
+package day1
+
+import util.parseLines
+
+/**
+ * Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+ *
+ * Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+ *
+ * two1nine
+ * eightwothree
+ * abcone2threexyz
+ * xtwone3four
+ * 4nineeightseven2
+ * zoneight234
+ * 7pqrstsixteen
+ * In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+ *
+ * What is the sum of all of the calibration values?
+ */
+fun partTwo(input: String): Sequence<Int> =
+    input.parseLines(::parse)
+        .map { TODO() }
+
+fun parse(line: String): List<Int> =
+    TODO()
+
+private val numbers =
+    mapOf(
+        "one" to 1,
+        "1" to 1,
+        "two" to 2,
+        "2" to 2,
+        "three" to 3,
+        "3" to 3,
+        "four" to 4,
+        "4" to 4,
+        "five" to 5,
+        "5" to 5,
+        "six" to 6,
+        "6" to 6,
+        "seven" to 7,
+        "7" to 7,
+        "eight" to 8,
+        "8" to 8,
+        "nine" to 9,
+        "9" to 9,
+        "zero" to 0,
+        "0" to 0,
+    )
